56bf3447b162c2b66cf43b8d9e65420b5ddf5d08
SavioJSilva <129877737+SavioJSilva@users.noreply.github.com>
2023-08-10 13:20:41 -0300

    adicionando ferramenta de cadastrar pessoas

--- Ex03pt1/Sistema/Sistema.py
+++ Ex03pt1/Sistema/Sistema.py
@@ -14,8 +14,13 @@ while True:
        # Opção de listar o conteúdo de um arquivo!
         lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabecalho('Saindo do sistema... Até logo!')
         break
     else:

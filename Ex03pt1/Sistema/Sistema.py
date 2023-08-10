1db1b5618f07bda5c13dacdd540a1f2715712313
SavioJSilva <129877737+SavioJSilva@users.noreply.github.com>
2023-08-10 12:52:30 -0300

    adicionando ferramente de criar e ler arquivo

--- Ex03pt1/Sistema/Sistema.py
+++ Ex03pt1/Sistema/Sistema.py
@@ -1,10 +1,18 @@
 from lib.interface import *
+from lib.Arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastras nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opção 1')
+       # cabecalho('Opção 1')
+       # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
         cabecalho('Opção 2')
     elif resposta == 3:
